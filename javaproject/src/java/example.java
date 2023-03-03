81a3940905f0b42179b794c9a75bea4bda3b6a3b
Your Name <you@example.com>
2023-03-03 10:06:45 +0530

    commit

---
+++ javaproject/src/java/example.java
@@ -0,0 +1,13 @@
+package java;
+
+public class example {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Java develeper:1");
+        }
+        System.out.println("*******************************");
+    }
+
+}
---
+++ javaproject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Aarti
+ *
+ */
+module javaproject {
+}
